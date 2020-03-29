b70e16bd2a2f29f35dd02bb77846e835476f3541
LeticiaWinter <leeewinter@hotmail.com>
2020-03-29 02:50:57 -0300

    BeckEnd live com teste de rota pelo Insominia

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const app = express();
+/// fizemos npm install nodemon - D e mudamos dentro do packge.json o test pra start: "nodemon index.js", depois demos npm start pro backend continuar live 
 
- app.get('/', (require,response) => { 
+
+const express = require('express'); //importando a dependencia
+const app = express(); //criando uma aplicação usando o express
+app.use(express.json()); //avisando que vamos usar json como objetos
+
+
+//criando a rota "users"
+ app.post('/users', (request,response) => { 
+     const body = request.body;
+
+     console.log(body);
+
      return response.json({evento: 'SemanaOmnistack',
     aluno: 'Lele'}) 
- }); //criando a rota principal
+ });
 
-app.listen(3333);
+
+ 
+app.listen(3333); //pra acessar, localhost:3333
